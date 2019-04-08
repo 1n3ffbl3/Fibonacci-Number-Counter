d770d5d9d6693d85f6b2f0850b2241be7a50787e
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-08 11:45:39 +0200

    Express setup with fibonacci endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const Fibonacci = require('./logic/fibonacci');
+const fibonacciCalculator = new Fibonacci();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/fibonacci/:n?', function (req, res) {
+	let fibonacciNumber = req.params.n;
+	console.log(`FibonacciNumber from Url: ${fibonacciNumber}`);
+	if (!fibonacciNumber) {
+		let currentMinute = new Date().getMinutes();
+		if (currentMinute < 10) {
+			fibonacciNumber = currentMinute;
+		} else {
+			fibonacciNumber = Number.parseInt(currentMinute.toString().substring(1));
+		}
+		console.log(`FibonacciNumber from Date: ${currentMinute} equals: ${fibonacciNumber}`);
+	}
+	res.status(200);
+	res.send({ fibonacciNumber: fibonacciCalculator.getFibonacci(fibonacciNumber) });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
